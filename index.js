d365b24bb15edff8f81e8c48974b4195ad308c03
Mohammad Ali <muhammadali.pass1@gmail.com>
2025-08-28 16:27:47 +0600

    Completed copy buttons functionality

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ function heartCountIncrease() {
     heartCountIncrease += 1
     document.getElementById("heart-count").innerText = heartCountIncrease;
 }
+
+function copyAndIncreaseCount(id) {
+    const textToCopy = document.getElementById(id).innerText;
+    let copyCountIncrease = parseInt(
+      document.getElementById("copy-count").innerText
+    );
+    navigator.clipboard.writeText(textToCopy);
+    alert(`নম্বর কপি হয়েছে: ${textToCopy}`);
+    copyCountIncrease += 1;
+    document.getElementById("copy-count").innerText = copyCountIncrease;
+ }
 // Heart icon functionality
 document.getElementById("heart-btn-1").addEventListener("click", function (e) {
     heartCountIncrease()
@@ -33,4 +44,34 @@ document.getElementById("heart-btn-8").addEventListener("click", function (e) {
 });
 document.getElementById("heart-btn-9").addEventListener("click", function (e) {
     heartCountIncrease()
+});
+
+
+// Copy button functionality
+document.getElementById("copy-btn-1").addEventListener("click", function (e) {
+    copyAndIncreaseCount("all-number")
+});
+document.getElementById("copy-btn-2").addEventListener("click", function (e) {
+    copyAndIncreaseCount("police-number");
+});
+document.getElementById("copy-btn-3").addEventListener("click", function (e) {
+    copyAndIncreaseCount("fire-number");
+});
+document.getElementById("copy-btn-4").addEventListener("click", function (e) {
+    copyAndIncreaseCount("ambulance-number");
+});
+document.getElementById("copy-btn-5").addEventListener("click", function (e) {
+    copyAndIncreaseCount("helpline-number");
+});
+document.getElementById("copy-btn-6").addEventListener("click", function (e) {
+    copyAndIncreaseCount("anti-corruption-number");
+});
+document.getElementById("copy-btn-7").addEventListener("click", function (e) {
+    copyAndIncreaseCount("electricity-number");
+});
+document.getElementById("copy-btn-8").addEventListener("click", function (e) {
+    copyAndIncreaseCount("brac-number");
+});
+document.getElementById("copy-btn-9").addEventListener("click", function (e) {
+    copyAndIncreaseCount("rail-number");
 });
